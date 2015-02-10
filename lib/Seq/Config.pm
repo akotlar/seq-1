e7054c31367149a2c33fc4ffa56d4175354bd694
Thomas Wingo <thomas.wingo@emory.edu>
2015-02-10 15:10:19 -0500

    updated Config.pm

--- lib/Seq/Config.pm
+++ lib/Seq/Config.pm
@@ -1,6 +1,10 @@
 package Seq::Config;
 
 use 5.10.0;
+use Exporter;
+use Moose;
+use namespace::autoclean;
+use Scalar::Util qw(reftype);
 use strict;
 use warnings;
 
@@ -16,10 +20,9 @@ Version 0.01
 
 our $VERSION = '0.01';
 
-
 =head1 SYNOPSIS
 
-Quick summary of what the module does.
+Set, get, and check configuration.
 
 Perhaps a little code snippet.
 
@@ -28,26 +31,130 @@ Perhaps a little code snippet.
     my $foo = Seq::Config->new();
     ...
 
-=head1 EXPORT
-
-A list of functions that can be exported.  You can delete this section
-if you don't export anything, such as for a purely object-oriented module.
-
-=head1 SUBROUTINES/METHODS
-
-=head2 function1
-
 =cut
 
-sub function1 {
+has chr_names => (
+  is => 'ro',
+  isa => 'ArrayRef[Str]',
+  traits => ['Array'],
+  required => 1,
+);
+has gene_track_annotation_names => (
+  is => 'ro',
+  isa => 'ArrayRef[Str]',
+  traits => ['Array'],
+  required => 1,
+);
+has gene_track_name => ( is => 'ro', isa => 'Str', required => 1, );
+has gene_track_statement => (is => 'ro', isa => 'Str', required => 1,);
+has genome_name => (is => 'ro', isa => 'Str', required => 1,);
+has genome_description => (is => 'ro', isa => 'Str', required => 1,);
+has phastCons_proc_clean_dir => (
+  is => 'ro',
+  isa => 'ArrayRef[Str]',
+  traits => ['Array'],
+  default => sub{ [] },
+);
+has phastCons_dir => (is => 'ro', isa => 'Str', required => 1,);
+has phastCons_files => (
+  is => 'ro',
+  isa => 'ArrayRef[Str]',
+  traits => ['Array'],
+  default => sub{ [] },
+);
+has phastCons_proc_chr => (
+  is => 'ro',
+  isa => 'ArrayRef[Str]',
+  traits => ['Array'],
+  default => sub{ [] },
+);
+has phastCons_proc_init => (
+  is => 'ro',
+  isa => 'ArrayRef[Str]',
+  traits => ['Array'],
+  default => sub{ [] },
+);
+has phyloP_proc_clean_dir => (
+  is => 'ro',
+  isa => 'ArrayRef[Str]',
+  traits => ['Array'],
+  default => sub{ [] },
+);
+has phyloP_dir => (is => 'ro', isa => 'Str', required => 1,);
+has phyloP_files => (
+  is => 'ro',
+  isa => 'ArrayRef[Str]',
+  traits => ['Array'],
+  default => sub{ [] },
+);
+has phyloP_proc_chr => (
+  is => 'ro',
+  isa => 'ArrayRef[Str]',
+  traits => ['Array'],
+  default => sub{ [] },
+);
+has phyloP_proc_init => (
+  is => 'ro',
+  isa => 'ArrayRef[Str]',
+  traits => ['Array'],
+  default => sub{ [] },
+);
+has seq_dir => (is => 'ro', isa => 'Str', required => 1,);
+has seq_files => (
+  is => 'ro',
+  isa => 'ArrayRef[Str]',
+  traits => ['Array'],
+  required => 1,
+);
+has seq_proc_init => (
+  is => 'ro',
+  isa => 'ArrayRef[Str]',
+  traits => ['Array'],
+  default => sub{ [] },
+);
+has snp_track_name => (is => 'ro', isa => 'Str', required => 1,);
+has snp_track_statement => (is => 'rw', isa => 'Str', required => 1,);
+
+sub BUILDARGS {
+  my $class = shift;
+  my $href = $_[0];
+  if (scalar @_ > 1 || reftype($href) ne "HASH")
+  {
+    confess "Error creating new Seq::Config, expected hash";
+  }
+  else
+  {
+    my %new_hash;
+    # set undefined attributes to "" for Str requiring attributes
+    for my $attr_name (qw( gene_track_name gene_track_statement genome_name
+      genome_description phastCons_dir phyloP_dir seq_dir snp_track_name
+      snp_track_statement))
+    {
+      $new_hash{$attr_name} //= $href->{$attr_name} || "";
+    }
+
+    # set undefined attributes as [] for ArrayRef requiring attributes
+    for my $attr_name (qw( chr_names gene_track_annotation_names
+      phastCons_proc_clean_dir phastCons_files phastCons_proc_chr
+      phastCons_proc_init phyloP_proc_clean_dir phyloP_files
+      phyloP_proc_chr phyloP_proc_init seq_files seq_proc_init ))
+    {
+      $new_hash{$attr_name} //= $href->{$attr_name} || [];
+    }
+    return $class->SUPER::BUILDARGS(\%new_hash);
+  }
 }
 
-=head2 function2
-
-=cut
-
-sub function2 {
-}
+around 'snp_track_statement' => sub {
+  my $orig = shift;
+  my $self = shift;
+  my @snp_table_fields     = qw( chrom chromStart chromEnd name
+                                 alleleFreqCount alleles alleleFreqs
+                               );
+  my $snp_table_fields_str = join(", ", @snp_table_fields);
+  (my $statement = $self->$orig(@_)) =~ s/\$fields/$snp_table_fields_str/;
+  return $statement;
+};
 
 =head1 AUTHOR
 
@@ -115,4 +222,6 @@ along with this program.  If not, see L<http://www.gnu.org/licenses/>.
 
 =cut
 
-1; # End of Seq::Config
+__PACKAGE__->meta->make_immutable;
+
+ 1; # End of Seq::Config
