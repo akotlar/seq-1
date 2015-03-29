fa4e9aa5c78bc39c364f61081055cb7fe866d0d5
Thomas Wingo <thomas.wingo@emory.edu>
2015-03-28 20:21:16 -0400

    minor edits to Seq::Role::Genome

--- lib/Seq/Role/Genome.pm
+++ lib/Seq/Role/Genome.pm
@@ -33,19 +33,19 @@ sub get_abs_pos {
   # then we're at the end of the genome so get the length of the genome; these
   # values are 0-indexed
   my $next_chr //= $class->get_next_chr($chr);
-  my $next_chr_start //= ($next_chr) ? $class->get_chr_len($next_chr) : $class->genome_length;
+  my $next_chr_start = ($next_chr) ? $class->get_chr_len($next_chr) : $class->genome_length;
 
   # when we call chr:pos we're using a 1-index genome; but str and char genoems
   # are 0-indexed so we subtract 1.
   my $abs_pos //= $class->get_chr_len($chr) + $pos - 1;
 
   if ($abs_pos > $next_chr_start) {
-    # getting end of chromosome; converting to 1-index for printing
+
+    # getting end of chromosome and converting to 1-index for printing
     my $chr_end = $next_chr_start - $class->get_chr_len($chr) + 1;
     croak "get_abs_pos(): site $pos is beyond end of the chr ($chr), which ends at $chr:$chr_end.\n";
   }
-
-
+  
   return $abs_pos;
 }
 
