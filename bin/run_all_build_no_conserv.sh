556ceca79708316b8fc74bcac76b27174b9aac91
Thomas Wingo <thomas.wingo@emory.edu>
2015-05-18 10:26:38 -0400

    removed old sh scripts

--- bin/run_all_build_no_conserv.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-./bin/build_genome_assembly.pl --config ./config/hg38_local_no_conserv.yml --location sandbox --type transcript_db &> hg38.transcript_db.log  &
-./bin/build_genome_assembly.pl --config ./config/hg38_local_no_conserv.yml --location sandbox --type snp_db &> hg38.snp_db.log &
-./bin/build_genome_assembly.pl --config ./config/hg38_local_no_conserv.yml --location sandbox --type gene_db &> hg38.gene_db.log
-./bin/build_genome_assembly.pl --config ./config/hg38_local_no_conserv.yml --location sandbox --type genome --hasher ./bin/genome_hasher &> hg38.genome.log
