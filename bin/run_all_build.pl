28351e3d467df35c2d8802faf5e1fc89f03074cd
Thomas Wingo <thomas.wingo@emory.edu>
2015-05-17 21:36:08 -0400

    added script to run all chr for gene and snp db creation

---
+++ bin/run_all_build.pl
@@ -0,0 +1,65 @@
+#!/usr/bin/env perl
+# Name:           run_all_build.pl
+# Date Created:   Sun May 17 21:10:27 2015
+# Date Modified:  Sun May 17 21:10:27 2015
+# By:             TS Wingo
+#
+# Description:
+
+
+use 5.10.0;
+use strict;
+use warnings;
+
+use lib './lib';
+use Carp;
+use Getopt::Long;
+use File::Spec;
+use Path::Tiny;
+use Pod::Usage;
+use Type::Params qw/ compile /;
+use Types::Standard qw/ :type /;
+use Log::Any::Adapter;
+use YAML::XS qw/ LoadFile /;
+
+# variables
+my ($verbose, $act, $config_file, $out_ext, $json_file, $data_ref);
+my @type = qw/ genome conserv transcript_db snp_db gene_db /;
+
+# get options
+die "Usage: $0 [-v] [-a] -b <build_assembly script> -c <assembly config> -l locaiton\n"
+  unless GetOptions(
+    'v|verbose' => \$verbose,
+    'a|act'     => \$act,
+    'f|file=s'  => \$config_file,
+    'j|json=s'  => \$json_file,
+    'o|out=s'   => \$out_ext,
+    ) and $config_file;
+$verbose++ unless $act;
+
+# clean path
+$build_assembly = path($build_assembly)->absolute->stringify;
+$config_file    = path($config_file)->absolute->stringify;
+$location       = path($location)->absolute->stringify;
+
+my $config_href = LoadFile( $config_file );
+
+for my $type ( qw/ gene_db snp_db / ) {
+  for my $chr ( @{ $config_href->{genome_chrs} } ) {
+    my $cmd = qq{$build_assembly --config $config_file --location $location --type $type --wanted_chr $chr}
+    $cmd .= " --verbose" if $verbose;
+    $cmd .= " --act" if $act;
+    my $file_name = Write_script( $type, $chr, $cmd );
+    my $q_cmd = qq{qsub -v USER -v PATH -cwd -o $type.$chr.log -j y $file_name}
+    say $q_cmd if $verbose;
+    system $q_cmd if $act;
+  }
+}
+
+sub Write_script {
+  my ( $type, $chr, $cmd ) = @_;
+  my $file_name = "$type.$chr.sh";
+  my $out_fh = IO::File->new( $file_name, 'w' );
+  say join "\n", '#!bin/sh', $cmd;
+  return $file_name;
+}
