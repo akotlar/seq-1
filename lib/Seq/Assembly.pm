a35974d098492c53d80e74df2c8935689edf169b
Thomas Wingo <thomas.wingo@emory.edu>
2015-07-04 17:55:42 -0400

    Edited documentation.
      - added MooX::Role::Logger
      - standardized error, warning, and infomation messages.

--- lib/Seq/Assembly.pm
+++ lib/Seq/Assembly.pm
@@ -5,11 +5,12 @@ use warnings;
 package Seq::Assembly;
 # ABSTRACT: A class for assembly information
 # VERSION
+
 =head1 DESCRIPTION
-  
+
   @class B<Seq::Assembly>
-  #TODO: Check description
-  
+  # TODO: Check description
+
   @example
 
 Used in: None
@@ -36,33 +37,39 @@ use Path::Tiny qw/ path /;
 use Seq::Config::GenomeSizedTrack;
 use Seq::Config::SparseTrack;
 
-use DDP;
+use DDP; # for debugging
 
-with 'Seq::Role::ConfigFromFile';
+with 'Seq::Role::ConfigFromFile', 'MooX::Role::Logger';
 
-#TODO: kotlar; I removed genome_db_dir, since what we really want is genome_index_dir, and that is already required
 my @_attributes = qw/ genome_name genome_description genome_chrs genome_index_dir
-      genome_hasher genome_scorer debug wanted_chr debug/; #removed genome_db_dir
+  genome_hasher genome_scorer debug wanted_chr debug/;
 
 has genome_name        => ( is => 'ro', isa => 'Str', required => 1, );
 has genome_description => ( is => 'ro', isa => 'Str', required => 1, );
+
+# TODO: kotlar; I commented out `genome_db_dir`, since what we really want is
+# `genome_index_dir`, and that is already required
 #has genome_db_dir      => ( is => 'ro', isa => 'Str', required => 1, );
 
 =property @public {Str} genome_index_dir
-  
-  The path (relative or absolute) to the index folder, which contains the binary reference genome file, chr offset file,
-  the 'gene' type database (current extension .kch, but could be any, like .dbm, depending on the database engine used),
-  binary 'score' type sparse_track files, 'score' type offset files, and 'cadd' type binary spare_track files.
 
-  Defined in the required input yaml config file, as a key : value pair, and is injected automatically by
+  The path (relative or absolute) to the index folder, which contains:
+  1) the binary GenomeSizedTrack of the reference genome, 2) the chr offset
+  file, 3) all SparseTrack database files, and 4) all binary GenomeSizedTrack
+  files of the optional 'score' and 'cadd' types.
+
+  Defined in the required input yaml config file, as a key : value pair, and
+  used by:
   @role Seq::Role::ConfigFromFile
-  
-  This is really a directory, and so is checked in Seq::Annotate::BUILDARGS
+
+  The existance of this directory is checked in Seq::Annotate::BUILDARGS
+
 @example genome_index_dir: ./hg38/index
 =cut
-has genome_index_dir   => ( is => 'ro', isa => 'Str', required => 1, );
 
-has genome_chrs        => (
+has genome_index_dir => ( is => 'ro', isa => 'Str', required => 1, );
+
+has genome_chrs => (
   is       => 'ro',
   isa      => 'ArrayRef[Str]',
   traits   => ['Array'],
@@ -96,12 +103,15 @@ has gene_tracks => (
     add_gene_track  => 'push',
   },
 );
+
 =property @public dbm_dry_run
 
-  Deprecated: If you just wanted to test annotation without the database engine locally installed.
-  Allowed you to skup writing a (KyotoCabinet or BerkleyDB) file. For testing.
+  Deprecated: If you just wanted to test annotation without the database engine
+  locally installed. Allowed you to skip writing a (KyotoCabinet or BerkleyDB)
+  database files.
 
 =cut
+
 has dbm_dry_run => (
   is      => 'ro',
   isa     => 'Bool',
@@ -124,30 +134,41 @@ sub BUILDARGS {
   else {
     my %hash;
 
-    # to avoid subtle chdir issues in a multi-user env, just make the genome_index_dir correct from the getgo
-    # $href->{genome_index_dir} =
-    #   path( $href->{genome_index_dir} )->absolute( $href->{genome_db_dir} );
-    # makes or returns undef, errors are trapped & exception thrown on error
-    # TODO: you're 100% right but this can be a bit cryptic when it happens ... i.e., how can the error msg
-    #       be helpful to the user?
+    $href->{genome_index_dir} = path( $href->{genome_index_dir} )->absolute;
 
-    # genome_db_dir is not needed here, since ->absolute will already use the cwd, which is changed into using
-    # -l | --location | genome_db_dir by functions that call Assembly.pm
-    $href->{genome_index_dir} = path( $href->{genome_index_dir} )->absolute; 
+    # is genome_index_dir a directory?
+    if ( !$href->{genome_index_dir}->is_dir ) {
+      # is the supplied genome_index_dir a file?
+      if ( $href->{genome_index_dir}->is_file ) {
+        my $err_msg =
+          sprintf( "ERROR: '%s' exists and is a file.", $href->{genome_index_dir} );
+        warn $err_msg . "\n";
+        $self->_logger->error($err_is_a_file);
+        exit 1;
+      }
 
-    if(!$href->{genome_index_dir}->is_dir)
-    {
-      confess("genome_index_dir is a file!!") if $href->{genome_index_dir}->is_file;
-      $href->{genome_index_dir}->mkpath or confess("Sorry, making the directory genome_index_dir failed!!");
+      # make the specified genome_index_dir.
+      if ( $href->{genome_index_dir}->mkpath ) {
+        my $err_msg = sprintf( "ERROR: failed to create genome_index_dir: '%s'",
+          $href->{genome_index_dir} );
+        warn $err_msg . "\n";
+        $self->_logger->error($err_msg);
+        exit 1;
+      }
     }
-    
+
     $href->{genome_index_dir} = $href->{genome_index_dir}->stringify;
 
-    say "The absolute genome_index_dir path is " .$href->{genome_index_dir} if $href->{debug};
-    for my $sparse_track ( @{ $href->{sparse_tracks} } ) 
-    {
-      $sparse_track->{genome_name} = $href->{genome_name};
-      #kotlar: subtle bug, Seq::Config::SparseTrack wasn't requiring genome_index_dir
+    if ( $href->{debug} ) {
+      my $msg =
+        sprintf( "The absolute genome_index_dir path is %s", $href->{genome_index_dir} );
+      say $msg;
+      $self->_logger->info($msg);
+    }
+
+    for my $sparse_track ( @{ $href->{sparse_tracks} } ) {
+
+      $sparse_track->{genome_name}      = $href->{genome_name};
       $sparse_track->{genome_index_dir} = $href->{genome_index_dir};
       if ( $sparse_track->{type} eq "gene" ) {
         push @{ $hash{gene_tracks} }, Seq::Config::SparseTrack->new($sparse_track);
@@ -159,16 +180,14 @@ sub BUILDARGS {
         croak sprintf( "unrecognized genome track type %s\n", $sparse_track->{type} );
       }
     }
-    for my $gst ( @{ $href->{genome_sized_tracks} } ) 
-    {
+    for my $gst ( @{ $href->{genome_sized_tracks} } ) {
       if ( $gst->{type} eq 'genome' or $gst->{type} eq 'score' or $gst->{type} eq 'cadd' )
       {
         $gst->{genome_chrs}      = $href->{genome_chrs};
         $gst->{genome_index_dir} = $href->{genome_index_dir};
 
-        if($href->{debug})
-        {
-          say "We are in the " .$gst->{type}." portion of the loop";
+        if ( $href->{debug} ) {
+          say "We are in the " . $gst->{type} . " portion of the loop";
           say "Here is what we are passing to Seq::Config::GenomeSizedTrack";
           p $gst;
         }
@@ -176,17 +195,11 @@ sub BUILDARGS {
 
         say "We got past Seq::Config::GenomeSizedTrack instantiation" if $href->{debug};
       }
-      # elsif ( $gst->{type} eq 'cadd' ) {
-      #   $gst->{genome_chrs}      = $href->{genome_chrs};
-      #   $gst->{genome_index_dir} = $href->{genome_index_dir};
-      #   $hash{cadd_track}        = Seq::Config::GenomeSizedTrack->new($gst);
-      # }
       else {
         croak sprintf( "unrecognized genome track type %s\n", $gst->{type} );
       }
     }
-    for my $attrib (@_attributes )
-    {
+    for my $attrib (@_attributes) {
       $hash{$attrib} = $href->{$attrib};
     }
     return $class->SUPER::BUILDARGS( \%hash );
