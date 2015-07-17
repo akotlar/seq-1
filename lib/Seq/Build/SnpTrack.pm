2c890bf279c86265efb481c53de4b6f9f85426dc
Thomas Wingo <thomas.wingo@emory.edu>
2015-07-17 18:25:52 -0400

    refactored to use Config::*

--- lib/Seq/Build/SnpTrack.pm
+++ lib/Seq/Build/SnpTrack.pm
@@ -47,122 +47,121 @@ sub build_snp_db {
 
   $self->_logger->info("starting to build snp db for chr: $wanted_chr");
 
-  # input
-  my $local_dir  = File::Spec->canonpath( $self->local_dir );
-  my $local_file = File::Spec->catfile( $local_dir, $self->local_file );
-  my $in_fh      = $self->get_read_fh($local_file);
+  # input files
+  my @input_files = $self->all_local_files;
 
-  # output
-  my $index_dir = File::Spec->canonpath( $self->genome_index_dir );
-  make_path($index_dir) unless -f $index_dir;
+  # prepare output dir, as needed
+  $self->genome_index_dir->mkpath unless ( -d $self->genome_index_dir );
 
-  # output: snp sites
-  my $snp_name = join( ".", $self->name, $wanted_chr, 'snp', 'dat' );
-  my $snp_file = File::Spec->catfile( $index_dir, $snp_name );
+  # get the names of the output files
+  my $snp_dat_file = $self->get_dat_file( $wanted_chr, $self->type );
+  my $dbm_file     = $self->get_kch_file( $wanted_chr );
 
   # check if we need to make the site range file
   #   skip build if site range file is present or we're forced to overwrite
-  return if $self->_has_site_range_file($snp_file) and !$self->force;
-  my $snp_fh;
+  return if $self->_has_site_range_file($snp_dat_file) and !$self->force;
 
-  my ( $dbm_name, $dbm_file, $db );
+  $self->_logger->info("Building entries for $wanted_chr");
 
-  $self->_logger->info("adding entries for $wanted_chr");
+  # variable to hold kch object and
+  my ( $db, $snp_dat_fh );
 
-  my ( %header, @snp_sites, @insert_data );
-  while ( my $line = $in_fh->getline ) {
-    chomp $line;
+  for my $input_file ( @input_files ) {
+    my $in_fh = $self->get_write_fh( $input_file );
+    my ( %header, @snp_sites, @insert_data );
 
-    my @fields = split /\t/, $line;
+    while ( my $line = $in_fh->geltine ) {
+      chomp $line;
 
-    # if there is no header hash assume we're at the begining of the file
-    if ( !%header ) {
-      %header = map { $fields[$_] => $_ } ( 0 .. $#fields );
+      my @fields = split /\t/, $line;
 
-      # do we have the essential keys?
-      $self->_check_header_keys( \%header, [qw/ chrom chromStart chromEnd name /] );
+      # if there is no header hash assume we're at the begining of the file
+      if ( !%header ) {
+        %header = map { $fields[$_] => $_ } ( 0 .. $#fields );
 
-      # do we have the optinally specified keys?
-      $self->_check_header_keys( \%header, [ $self->all_features ] );
+        # do we have the essential keys?
+        $self->_check_header_keys( \%header, [qw/ chrom chromStart chromEnd name /] );
 
-      # create dbm file
-      $dbm_name = join ".", $self->name, $wanted_chr, $self->type, 'kch';
-      $dbm_file = File::Spec->catfile( $index_dir, $dbm_name );
-      $self->_logger->info("dbm_file: $dbm_file");
-      $db = Seq::KCManager->new(
-        filename => $dbm_file,
-        mode     => 'create',
-        # chosed as ~ 50% of the largest number of SNPs on a chr (chr 2)
-        bnum => 3_000_000,
-        msiz => 512_000_000,
-      );
+        # do we have the optinally specified keys?
+        $self->_check_header_keys( \%header, [ $self->all_features ] );
 
-      # create site-range file
-      #   NOTE: 1st line needs to be value that should be added to encoded
-      #         genome for these sites
-      $snp_fh = $self->get_write_fh($snp_file);
-      say {$snp_fh} $self->in_snp_val;
-    }
+        # create dbm file
+        $self->_logger->info("dbm_file: $dbm_file");
+        $db = Seq::KCManager->new(
+          filename => $dbm_file,
+          mode     => 'create',
+          # chosed as ~ 50% of the largest number of SNPs on a chr (chr 2)
+          bnum => 3_000_000,
+          msiz => 512_000_000,
+        );
 
-    # process wanted chr
-    next unless $fields[0] eq $wanted_chr;
-
-    my %data = map { $_ => $fields[ $header{$_} ] } @{ $self->snp_fields_aref };
-    my ( $allele_freq_count, @alleles, @allele_freqs, $min_allele_freq );
-
-    if ( $data{alleleFreqCount} ) {
-      @alleles      = split( /,/, $data{alleles} );
-      @allele_freqs = split( /,/, $data{alleleFreqs} );
-      my @s_allele_freqs = sort { $b <=> $a } @allele_freqs;
-      $min_allele_freq = sprintf( "%0.6f", 1 - $s_allele_freqs[0] );
-    }
-
-    foreach my $pos ( ( $data{chromStart} + 1 ) .. $data{chromEnd} ) {
-      my $chr      = $data{chrom};
-      my $snp_id   = $data{name};
-      my $abs_pos  = $self->get_abs_pos( $chr, $pos );
-      my $base     = $self->get_base( $abs_pos, 1 );
-      my $snp_site = Seq::Site::Snp->new(
-        {
-          abs_pos  => $abs_pos,
-          snp_id   => $snp_id,
-          ref_base => $base,
-        }
-      );
-
-      my %feature_hash = map { $_ => $data{$_} } ( $self->all_features );
-
-      # this is a total hack - MAF might be nice to have but doesn't fit into the
-      # present framework well since it's not a 'feature' we retrieve but rather
-      # it's calculated... since you get about the same info with alleleFreqs
-      # I'm not really sure it's even needed.
-      $feature_hash{maf} = $min_allele_freq if ($min_allele_freq);
-
-      $snp_site->set_snp_feature(%feature_hash);
-
-      push @snp_sites, $abs_pos;
-
-      my $site_href = $snp_site->as_href;
-
-      $db->db_put( $abs_pos, $site_href );
-
-      if ( $self->counter > $self->bulk_insert_threshold ) {
-        say {$snp_fh} join "\n", @{ $self->_get_range_list( \@snp_sites ) };
-        @snp_sites = ();
-        $self->reset_counter;
+        # create site-range file
+        #   NOTE: 1st line needs to be value that should be added to encoded
+        #         genome for these sites
+        $snp_dat_fh = $self->get_write_fh($snp_dat_file);
+        say {$snp_dat_fh} $self->in_snp_val;
       }
+
+      # process wanted chr
+      next unless $fields[0] eq $wanted_chr;
+
+      my %data = map { $_ => $fields[ $header{$_} ] } @{ $self->snp_fields_aref };
+      my ( $allele_freq_count, @alleles, @allele_freqs, $min_allele_freq );
+
+      if ( $data{alleleFreqCount} ) {
+        @alleles      = split( /,/, $data{alleles} );
+        @allele_freqs = split( /,/, $data{alleleFreqs} );
+        my @s_allele_freqs = sort { $b <=> $a } @allele_freqs;
+        $min_allele_freq = sprintf( "%0.6f", 1 - $s_allele_freqs[0] );
+      }
+
+      foreach my $pos ( ( $data{chromStart} + 1 ) .. $data{chromEnd} ) {
+        my $chr      = $data{chrom};
+        my $snp_id   = $data{name};
+        my $abs_pos  = $self->get_abs_pos( $chr, $pos );
+        my $base     = $self->get_base( $abs_pos, 1 );
+        my $snp_site = Seq::Site::Snp->new(
+          {
+            abs_pos  => $abs_pos,
+            snp_id   => $snp_id,
+            ref_base => $base,
+          }
+        );
+
+        my %feature_hash = map { $_ => $data{$_} } ( $self->all_features );
+
+        # this is a total hack - MAF might be nice to have but doesn't fit into the
+        # present framework well since it's not a 'feature' we retrieve but rather
+        # it's calculated... since you get about the same info with alleleFreqs
+        # I'm not really sure it's even needed.
+        $feature_hash{maf} = $min_allele_freq if ($min_allele_freq);
+
+        $snp_site->set_snp_feature(%feature_hash);
+
+        push @snp_sites, $abs_pos;
+
+        my $site_href = $snp_site->as_href;
+
+        $db->db_put( $abs_pos, $site_href );
+
+        if ( $self->counter > $self->bulk_insert_threshold ) {
+          say {$snp_dat_fh} join "\n", @{ $self->_get_range_list( \@snp_sites ) };
+          @snp_sites = ();
+          $self->reset_counter;
+        }
+      }
+      $self->inc_counter;
     }
-    $self->inc_counter;
-  }
-  if ( $self->counter ) {
-    say {$snp_fh} join "\n", @{ $self->_get_range_list( \@snp_sites ) };
-    @snp_sites = ();
-    $self->reset_counter;
+    if ( $self->counter ) {
+      say {$snp_dat_fh} join "\n", @{ $self->_get_range_list( \@snp_sites ) };
+      @snp_sites = ();
+      $self->reset_counter;
+    }
   }
 
   # add a final blank line to the region file; this is a bit of a hack so the c
   # hasher will not crash if there are no entries (after the initial idx mask)
-  say {$snp_fh} '';
+  say {$snp_dat_fh} '';
   $self->_logger->info("finished building snp site db for chr: $wanted_chr");
 }
 
