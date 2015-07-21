53eae6323d2fae8489b985a230f1eb894a6e2212
Thomas Wingo <thomas.wingo@emory.edu>
2015-07-21 16:56:04 -0400

    fixed typos

--- lib/Seq/Config/GenomeSizedTrack.pm
+++ lib/Seq/Config/GenomeSizedTrack.pm
@@ -281,7 +281,7 @@ sub _build_raw_genome_files {
   return \@array;
 }
 
-= property @public cadd_idx_file
+=property @public cadd_idx_file
 
   This function is intented to be called from an annotate context; therefore,
   it will check for the existance of the file.
@@ -293,7 +293,7 @@ sub cadd_idx_file {
   my $file = $self->genome_bin_file->absolute->stringify . "." . $num;
   if (!-f $file ) {
     my $msg = sprintf("ERROR: cannot find expected cadd-type file '%s'", $file);
-    $self->_logger->error($msg)
+    $self->_logger->error($msg);
     say $msg;
     exit(1);
   }
