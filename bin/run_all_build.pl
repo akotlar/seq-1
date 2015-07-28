a03c964502fb88b855a330292d1ba6b95e14646f
Thomas Wingo <thomas.wingo@emory.edu>
2015-07-27 21:55:44 -0400

    minor work arounds for cluster
    Former-commit-id: 433cd2ad5c76f8c6668307aeb1e3eb193295159d

--- bin/run_all_build.pl
+++ bin/run_all_build.pl
@@ -21,38 +21,43 @@ use Log::Any::Adapter;
 use YAML::XS qw/ LoadFile /;
 
 # variables
-my ( $verbose, $act, $config_file, $out_ext, $build_scr );
+my ( $verbose, $act, $config_file, $out_ext, $build_src );
 my $cwd  = cwd();
 my @type = qw/ genome conserv transcript_db snp_db gene_db /;
 
 # get options
-die
-  "Usage: $0 [-v] [-a] -b <build_assembly script> -c <assembly config> -l location\n"
+die "Usage: $0 [-v] [-a] -b <build_assembly script> -c <assembly config>\n"
   unless GetOptions(
   'v|verbose'  => \$verbose,
   'a|act'      => \$act,
-  'b|build=s'  => \$build_scr,
+  'b|build=s'  => \$build_src,
   'c|config=s' => \$config_file,
   ) and $config_file;
 $verbose++ unless $act;
 
 # clean path
-$build_scr   = path($build_scr)->absolute->stringify;
+$build_src   = path($build_src)->absolute->stringify;
 $config_file = path($config_file)->absolute->stringify;
 
 my $config_href = LoadFile($config_file);
 
-my $cmd_fh = IO::File->new( 'build.sh', 'w' );
+my $cmd_fh = IO::File->new( 'build.sh', 'w' ) or die "build.sh: $!\n";
+my $alt_cmd_fh = IO::File->new( 'build_alt.sh', 'w') or die "build_alt.sh: $!\n";
+my $i = 1;
 
 for my $type (qw/ gene_db snp_db /) {
   for my $chr ( @{ $config_href->{genome_chrs} } ) {
-    my $cmd = qq{$build_scr --config $config_file --type $type --wanted_chr $chr};
+    my $cmd = qq{$build_src --config $config_file --type $type --wanted_chr $chr};
     $cmd .= " --verbose" if $verbose;
     $cmd .= " --act"     if $act;
     my $file_name = Write_script( $type, $chr, $cmd );
+    my $alt_cmd = sprintf("%s %s", path('.')->child($file_name)->absolute->stringify, 
+      ( $i % 3 == 0 ) ? "" : " &");
+    say { $alt_cmd_fh } $alt_cmd;
+    $i++;
     my $log_file  = File::Spec->rel2abs("$type.$chr.log");
     my $q_cmd     = qq{qsub -v USER -v PATH -cwd -q lh.q -o $log_file -j y $file_name};
-    say {$cmd_fh} $q_cmd if $verbose;
+    say {$cmd_fh} $q_cmd;
   }
 }
 
