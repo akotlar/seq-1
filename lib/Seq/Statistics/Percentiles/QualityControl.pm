fa52daedbd002917c68decb5aae20d787d2cebe6
akotlar <akotlar@bu.edu>
2015-10-28 14:26:16 -0400

    added remaining statistics packages

---
+++ lib/Seq/Statistics/Percentiles/QualityControl.pm
@@ -0,0 +1,41 @@
+package Seq::Statistics::Percentiles::QualityControl;
+
+use Moose::Role;
+use strict;
+use warnings;
+
+requires 'getPercentile';
+requires 'qcFailKey';
+requires 'ratioName';
+
+has ciMessage =>
+( 
+  is      => 'ro',
+  isa     => 'Str',
+  default => 'outside 95th percentile'
+);
+
+sub qc
+{
+  my ($self, $keysAref, $valuesAref, $destHref) = @_;
+
+  my $qcFailKey = $self->qcFailKey;
+  my $ciMessage = $self->ciMessage;
+  my $ratioName = $self->ratioName;
+
+  my $lower = $self->getPercentile(0);
+  my $upper = $self->getPercentile(2);
+  
+  my $index = 0;
+  for my $ratio (@$valuesAref)
+  {
+    if($ratio < $lower || $ratio > $upper)
+    {
+      $destHref->{$qcFailKey}{$keysAref->[$index] } = "$ratioName $ciMessage";
+    }
+    $index++;
+  }
+}
+
+no Moose::Role;
+1;
---
+++ lib/Seq/Statistics/Record.pm
@@ -0,0 +1,124 @@
+package Seq::Statistics::Record;
+
+use Moose::Role;
+use strict;
+use warnings;
+
+use Carp qw/cluck confess/;
+use List::MoreUtils qw(first_index);
+
+use DDP;
+
+requires 'deconvoluteIUPAC';
+requires 'isDisallowedGeno';
+requires 'statsKey';
+requires 'statsRecord';
+requires 'debug';
+
+has countKey => 
+(
+  is => 'rw',
+  isa => 'Str',
+  lazy => 1,
+  default => 'count',
+);
+
+#href because the value corrsponds to the indices of _transitionTranvserionKeysAref
+has _transitionTypesHref =>
+( is      => 'ro',
+  isa     => 'HashRef[Int]',
+  traits  => ['Hash'],
+  handles => {
+    isTr => 'get',
+  },
+  default => sub { {AG => 1,GA => 1,CT => 1,TC => 1,R => 1,Y => 1} },
+  lazy => 1,
+  init_arg => undef 
+);
+
+has _transitionTransversionKeysAref =>
+( is      => 'ro',
+  isa     => 'ArrayRef[Str]',
+  traits  => ['Array'],
+  handles => {
+    trTvKey => 'get',
+  },
+  default => sub{ ['Transversions','Transitions'] },
+  lazy => 1,
+  init_arg => undef,
+);
+
+# at every level in the has, record whether transition or transversion
+# assumes that only non-reference alleles are passed, hence it is a role
+sub record
+{
+  my ($self, $sampleIDgenoHref, $annotationsAref, $refAllele,
+    $varType, $genomicType) = @_;
+  my ($geno, $deconvolutedGeno, @countedCombos, $combo, $minorAllele,
+    $annotationType, $sampleStats, $trTvKey);
+
+  for my $sampleID (keys %$sampleIDgenoHref)
+  {
+    $geno = $sampleIDgenoHref->{$sampleID};
+    #in case not passed carriers/hom mut's
+    if($geno eq $refAllele|| $self->badGenos(sub{$_ eq $geno} > -1 ) ) {next; } 
+    
+    $deconvolutedGeno = $self->deconvoluteIUPAC($geno);
+    for my $annotationHref (@$annotationsAref)
+    {
+      $minorAllele = $annotationHref->minor_allele;
+      if(index($deconvolutedGeno, $minorAllele) > -1) {next;}
+     
+      $annotationType = $annotationHref->annotation_type;
+      $combo = $minorAllele. $annotationHref->annotation_type;
+
+      #count each allele/feature combo one to fairly attribute multi-allelic variants
+      #to samples that have one of the minor alleles but not the other (when both represented)
+      if(first_index { $_ eq $combo } @countedCombos > -1) {next;}
+      push @countedCombos, $combo;
+
+      $sampleStats = $self->getStats($sampleID);
+
+      $trTvKey = $self->_getTr($refAllele, $geno);
+
+      $self->storeTrTvAndCount([$varType, $genomicType, $annotationType],
+        $sampleStats, $trTvKey);
+    }
+  }
+  p $self->statsRecord if $self->debug;
+}
+
+#topTargetHref remains the top level hash, always gets transitions/transversion record
+#because we want genome-wide tr:tv
+sub storeTrTvAndCount
+{
+  my ($self, $featuresAref, $topTargetHref, $trTvKey, $targetHref) = @_;
+
+  if(!scalar @$featuresAref) { return };
+
+  $targetHref = defined $targetHref ? $targetHref : $topTargetHref;
+
+  my $feature = shift @$featuresAref;
+  my $statHref = \%{$targetHref->{$feature}{$self->statsKey} };
+
+  $statHref->{$trTvKey} += 1;
+  $topTargetHref->{$self->statsKey}{$trTvKey} += 1; #genome-wide tr:tv
+  $statHref->{$self->countKey} += 1;
+
+  $self->storeTrTvAndCount($featuresAref, $topTargetHref, $trTvKey,
+    $targetHref->{$feature});
+}
+
+sub _getTr
+{
+  my ($self, $refAllele, $geno) = @_;
+
+  #heterozygotes
+  if($self->trTvKey($self->isTr($geno) )
+  # homozygotes
+  || ($self->trTvKey($refAllele.$geno) ) )
+  {
+    return 1;
+  }
+  return 0;
+}
