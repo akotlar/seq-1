1150d2bb103e89336b23243b1a81fb1abe158fa3
Alex Kotlar <akotlar@node01.local>
2015-11-11 18:29:57 -0500

    having issue with Seq::Site::Annotation constraint, in getting data out of Annotate.pm through Seq::Annotate:All

--- lib/Seq/Site/Indel.pm
+++ lib/Seq/Site/Indel.pm
@@ -10,18 +10,19 @@ our $VERSION = '0.001';
 # VERSION
 
 use Moose 2;
-use Moose::Util::TypeConstraints;
 
 use namespace::autoclean;
-
-use Data::Dump qw/ dump /;
+use Moose::Util::TypeConstraints;
+use DDP;
 
 extends 'Seq::Site::Gene';
 with 'Seq::Role::Serialize';
+with 'Seq::Role::Message';
 
+#can't use type str, because -1 is a number, could coerce, but why?
+#will fail early if input bad anyway
 has minor_allele => (
   is       => 'ro',
-  isa      => 'Maybe[Str]',
   required => 1,
 );
 
@@ -40,17 +41,46 @@ has new_aa_residue => (
 );
 
 has annotation_type => (
-  is       => 'ro',
+  is       => 'rw',
   isa      => 'Str',
-  required => 1,
+  lazy     => 1,
+  builder  => '_set_annotation_type'
 );
 
-#I think these should use Seq::Role::Genotypes to get the codon seq
+enum indTypes => [qw(- +)];
+has indType => (
+  is => 'ro',
+  isa => 'indTypes',
+  lazy => 1,
+  builder => '_build_indel_type',
+);
+
+#@returns - or +
+sub _build_indel_type {
+  my $self = shift;
+  #cast as str to substr in case of -N
+  return substr("".$self->minor_allele, 0, 1); 
+}
+
+has indLength => (
+  is => 'ro',
+  isa => 'Num',
+  lazy => 1,
+  builder => '_build_indel_length',
+);
+
+sub _build_indel_length {
+  my $self = shift;
+  return substr($self->minor_allele, 1) if $self->indType eq '-'; #a number
+  return length($self->minor_allele) - 1 if $self->indType eq '+'; #a string
+}
+#Thomas, haven't done anything with the next 2 methods yet
+#have $self->indType if want to use this
 sub _set_new_codon_seq {
   my $self = shift;
 
   if ( $self->ref_codon_seq ) {
-    return '-';
+    return $self->indType . $self->indLength;
   }
   else {
     return;
@@ -61,11 +91,38 @@ sub _set_new_aa_residue {
   my $self = shift;
 
   if ( $self->new_codon_seq ) {
-    return '-';
+    return $self->indType . $self->indLength;
   }
   else {
     return;
   }
+}
+
+sub BUILD {
+  my $self = shift;
+
+  $self->annotation_type;
+};
+
+state $frames = ['InFrame', 'FrameShift'];
+sub _set_annotation_type {
+  my $self = shift;
+  my $frame = $frames->[$self->indLength % 3];
+  my $str = $self->indType eq '-' ? 'Del' : 'Ins' ."-$frame-";
+  #first capture gross
+  #covers 3UTR, 5UTR, and all other GeneSiteType 's enum'd
+  my $annotation_type = $str . $self->site_type . ";"; #or could interpolate ${}
+
+  if($self->site_type eq 'Coding') {
+    if($self->codon_number == 1) {
+      $annotation_type .= $str . "StartLoss;"; #or could interpolate
+    }
+    if ($self->ref_aa_residue eq '*' ) {
+      $annotation_type .= $str . "StopLoss;";
+    }
+  }
+  chop $annotation_type;
+  return $annotation_type;
 }
 
 __PACKAGE__->meta->make_immutable;
